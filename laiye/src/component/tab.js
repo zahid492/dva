5d19e9773ea0f40f885ae187c890d23f5de461d2
disnio <291166364@qq.com>
2018-03-05 17:40:46 +0800

    echart zrender 模拟
    Signed-off-by: disnio <291166364@qq.com>

--- laiye/src/component/tab.js
+++ laiye/src/component/tab.js
@@ -1,34 +1,89 @@
-import React, { Component } from 'react';
-import { Tabs, WhiteSpace } from 'antd-mobile';
-import List from './list';
-import DList from './dlist';
+import React, {Component} from 'react';
+import {Tabs, WhiteSpace} from 'antd-mobile';
 
+// import List from './list';
+// import DList from './dlist';
+// tab 横向切换，多 tab 嵌套测试
 export default class Tab extends Component {
-    renderContent = tab =>
-        (<div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '150px', backgroundColor: '#fff' }}>
+    renderContent = tab => {
+        return (<div style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '150px',
+            backgroundColor: '#fff'
+        }}>
             <p>Content of {tab.title}</p>
-        </div>);
+        </div>)
+    };
+
+    renderCt = tab => {
+        const tb = {
+            tabs1: [
+                {title: '1-1st Tab'},
+                {title: '1-2nd Tab'},
+                {title: '1-3rd Tab'},
+                {title: '1-4th Tab'},
+                {title: '1-5th Tab'},
+                {title: '1-6th Tab'},
+            ],
+            tabs2: [
+                {title: '2-1st Tab'},
+                {title: '2-2nd Tab'},
+                {title: '2-3rd Tab'},
+                {title: '2-4th Tab'},
+                {title: '2-5th Tab'},
+            ],
+            tabs3: [
+                {title: '3-1st Tab'},
+                {title: '3-2nd Tab'},
+                {title: '3-3rd Tab'},
+                {title: '3-4th Tab'},
+            ]
+        };
+        const tbk = "tabs" + tab.id;
+        return (
+            <div>
+                <Tabs tabs={tb[tbk]} swipeable={true}>
+                    {this.renderContent}
+                </Tabs>
+            </div>
+        )
+    };
 
     render() {
-        const tabs = [
-            { title: '1st Tab' },
-            { title: '2nd Tab' },
-            { title: '3rd Tab' },
-            // { title: '4th Tab' },
-            // { title: '5th Tab' },
-            // { title: '6th Tab' },
-            // { title: '7th Tab' },
-            // { title: '8th Tab' },
-            // { title: '9th Tab' },
+
+        const tabt = [
+            {title: '111st Tab', id: 1},
+            {title: '222nd Tab', id: 2},
+            {title: '333rd Tab', id: 3},
         ];
 
         return (
             <div>
-                <WhiteSpace />
-                <Tabs tabs={tabs} swipeable={false}>
-                    <DList />
+                <WhiteSpace/>
+                <Tabs tabs={tabt} swipeable={false} className={"xxx"}
+                      renderTabBar={(props) => {
+                          return (
+                              <div className="ggg">
+                                  <div style={{color: "red", float: "left"}}>Left</div>
+                                  <div style={{width: "275px", float: "left"}}>
+                                      <Tabs.DefaultTabBar
+                                          {...props}
+                                          renderTab={(tab) => {
+                                              return <div>{tab.title}</div>;
+                                          }}
+                                      />
+                                  </div>
+                                  <div style={{color: "blue", float: "left"}}>Right</div>
+                              </div>
+                          )
+                      }}
+                >
+                    {this.renderCt}
                 </Tabs>
-                <WhiteSpace />
+
+                <WhiteSpace/>
             </div>
         );
     }
--- laiye/src/containers/App.js
+++ laiye/src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import ProductList from '../component/ProductList'
 import InputBox from '../component/Input'
+import List from '../component/tab'
 
 // import asyncComponent from '../component/AsyncComponent'
 // import LineReact from '../component/charts'
@@ -10,8 +11,10 @@ export default class App extends Component {
     render() {
         return (
             <div>
-                <InputBox />
-                <ProductList />
+                {/*<InputBox />*/}
+                {/*<ProductList />*/}
+                <List />
+
             </div>
         )
     }
