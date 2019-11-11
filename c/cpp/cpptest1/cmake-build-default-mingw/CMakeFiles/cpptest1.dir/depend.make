66f7700b2804bc9af089690bf53643a5ecdca513
disnio <291166364@qq.com>
2019-11-11 12:55:17 +0800

    c in hc
    Signed-off-by: disnio <291166364@qq.com>

---
+++ c/cpp/cpptest1/cmake-build-default-mingw/CMakeFiles/cpptest1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cpptest1.
+# This may be replaced when dependencies are built.
---
+++ c/cpp/cpptest1/cmake-build-default/CMakeFiles/cpptest1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpptest1.dir/cpptest.cpp.obj"
+  "cpptest1.pdb"
+  "cpptest1.exe"
+  "cpptest1.exe.manifest"
+  "libcpptest1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpptest1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
